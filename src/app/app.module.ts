262a9eb6db76e05e61f26543f4d6d59d8f1d75ed
Rupam <rupampal48@gmail.com>
2023-12-20 19:19:42 +0530

    1st com - Code done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstModuleModule } from './first-module/first-module.module';
+import { SecondModuleModule } from './second-module/second-module.module';
+import { CommunicationService } from './services/communication.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FirstModuleModule,
+    SecondModuleModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [CommunicationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
